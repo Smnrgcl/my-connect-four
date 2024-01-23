7a5eec496c4827b2b04fb4ad4a596157184eae6b
semanurgüçlü <smnrguclu06@gmail.com>
2024-01-23 15:02:21 +0300

    The list is also set to save the same game once

--- src/components/ListOfGamesScreen.js
+++ src/components/ListOfGamesScreen.js
@@ -2,10 +2,27 @@ import React from 'react';
 
 const ListOfGamesScreen = () => {
   // Local storage'tan oyun bilgi dizisini al
-  const gameInfoArray = JSON.parse(localStorage.getItem('gameInfoArray')) || [];
+  let gameInfoArray = JSON.parse(localStorage.getItem('gameInfoArray')) || [];
+
+  // Aynı isme sahip olan oyunların son kazananlarını güncelle
+  const updatedGameInfoArray = [];
+  const uniqueGames = new Set();
+  gameInfoArray.reverse(); // En son oynanan oyunları ilk önce işleyelim
+
+  gameInfoArray.forEach((oyunBilgisi) => {
+    if (!uniqueGames.has(oyunBilgisi.gameName)) {
+      // Aynı isme sahip oyunu ilk defa görüyoruz, kazananı güncelle
+      uniqueGames.add(oyunBilgisi.gameName);
+      const lastWinner = getLastWinner(oyunBilgisi.gameName);
+      if (lastWinner) {
+        oyunBilgisi.winner = lastWinner;
+      }
+      updatedGameInfoArray.push(oyunBilgisi);
+    }
+  });
 
   // Diziden son 10 oyunu al
-  const son10Oyunlar = gameInfoArray.slice(Math.max(gameInfoArray.length - 10, 0));
+  const son10Oyunlar = updatedGameInfoArray.slice(0, Math.min(updatedGameInfoArray.length, 10));
 
   const containerStyle = {
     display: 'flex',
@@ -24,7 +41,15 @@ const ListOfGamesScreen = () => {
     textAlign: 'left',
   };
 
- 
+  // Oyun adına göre son kazananı bulan yardımcı fonksiyon
+  function getLastWinner(gameName) {
+    for (let i = 0; i < gameInfoArray.length; i++) {
+      if (gameInfoArray[i].gameName === gameName) {
+        return gameInfoArray[i].winner;
+      }
+    }
+    return null;
+  }
 
   return (
     <div style={containerStyle}>
